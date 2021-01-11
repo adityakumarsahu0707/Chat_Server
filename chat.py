42e67a47daafb5f921130c680b72fddf8424028d
Aditya Kumar Sahu <46383854+adityakumarsahu0707@users.noreply.github.com>
2021-01-11 23:49:27 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,30 @@
+import threading
+import socket
+import time
+
+
+def send():
+
+  while True:
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    text=input()
+    text=text.encode()
+    s.sendto(text, ("192.168.43.114", 1234))
+    print("\n")
+#    time.sleep(5)
+
+def recieve():
+  while True:
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip="192.168.43.193"
+    port=1235
+    s.bind((ip,port))
+    x=s.recvfrom(100)
+    rep=x[0].decode()
+    print("Reply recieved from " + x[1][0] + " : " + rep + "\n")
+
+t1 = threading.Thread(target=send)
+t2 = threading.Thread(target=recieve)
+
+t1.start()
+t2.start()
